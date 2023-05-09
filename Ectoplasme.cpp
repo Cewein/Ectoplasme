67fdff0716be763c4222b3df8edc658ee2e6209e
Maximilien Nowak <pro.mnowak@gmail.com>
2023-05-09 22:59:33 +1000

    init cmake

---
+++ Ectoplasme.cpp
@@ -0,0 +1,12 @@
+﻿// Ectoplasme.cpp : Defines the entry point for the application.
+//
+
+#include "Ectoplasme.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
