5a6bb9d707248759ca680c789bc2a8b284c33a8b
Maximilien Nowak <pro.mnowak@gmail.com>
2023-10-22 22:38:28 +0200

    remove vulkan
    Well, here is just for prototyping.
    Having a fully fledge vulkan application is not the goal yet.
    
    First a proto then upgrade everything to vulkan. OpenGL is ok for the beggining

--- Ectoplasme.cpp
+++ Ectoplasme.cpp
@@ -1,5 +1,4 @@
-﻿#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
+﻿#include <GLFW/glfw3.h>
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
@@ -8,8 +7,6 @@
 
 #include <iostream>
 #include <vector>
-
-#include <vulkan/vulkan.h>
 
 int main()
 {
@@ -23,86 +20,6 @@ int main()
 
 	GLFWwindow* window = glfwCreateWindow(width, height, "Ectoplasme", nullptr, nullptr);
 
-	//setup validation layer trough macro
-
-	const std::vector<const char*> validationLayers = 
-	{
-		"VK_LAYER_KHRONOS_validation"
-	};
-
-#ifdef NDEBUG
-	const bool enableValidationLayers = false;
-#else
-	const bool enableValidationLayers = true;
-#endif // NDEBUG
-
-	//create vulkan instance to use the api
-	VkInstance vkInstance;
-	VkApplicationInfo appInfo{};
-
-	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO; 
-	appInfo.pApplicationName = "Ectoplasme";
-	appInfo.applicationVersion = VK_MAKE_VERSION(0, 0, 1);
-	appInfo.pEngineName = "ShinyCore";
-	appInfo.engineVersion = VK_MAKE_VERSION(0, 0, 1);
-	appInfo.apiVersion = VK_API_VERSION_1_0;
-
-	//check the validation layer for the instance
-	unsigned int layerCount;
-	vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
-	std::vector<VkLayerProperties> availableLayers(layerCount);
-	vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
-
-	//define as true since no layer can be requested
-	//then it must pass
-	bool layerFound = true;
-
-	//check if the layers requested are present
-	for (int i = 0; i < validationLayers.size(); i++)
-	{
-		layerFound = false;
-
-		for (int j = 0; j < availableLayers.size(); j++)
-		{
-			//compart the two layer name to see if the match
-			if (strcmp(validationLayers[i], availableLayers[j].layerName) == 0)
-			{
-				layerFound = true;
-				break;
-			}
-		}
-
-		if (!layerFound) break;
-	}
-
-	if (enableValidationLayers && !layerFound) throw std::runtime_error("Validation layers requested, but not available!");
-
-	//create info for the vulkan instance
-	VkInstanceCreateInfo createInfo{};
-	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-	createInfo.pApplicationInfo = &appInfo;
-
-	unsigned int glfwExtensionCount = 0;
-	const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-
-	createInfo.enabledExtensionCount = glfwExtensionCount;
-	createInfo.ppEnabledExtensionNames = glfwExtensions;
-	createInfo.enabledExtensionCount = 0;
-
-	//add info about the validation layer
-	if (enableValidationLayers)
-	{
-		createInfo.enabledLayerCount = unsigned int(validationLayers.size());
-		createInfo.ppEnabledLayerNames = validationLayers.data();
-	}
-	else createInfo.enabledLayerCount = 0;
-	
-	VkResult result = vkCreateInstance(&createInfo, nullptr, &vkInstance);
-
-	if(result != VK_SUCCESS) throw std::runtime_error("Vulkan instant failed to be created");
-
-	//instance 
-
 	//main loop like in opengl
 	while (!glfwWindowShouldClose(window))
 	{
@@ -110,7 +27,6 @@ int main()
 	}
 	
 	// clean up
-	vkDestroyInstance(vkInstance, nullptr);
 	glfwDestroyWindow(window);
 	glfwTerminate();
 
