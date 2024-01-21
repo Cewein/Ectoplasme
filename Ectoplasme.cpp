9275c1ee3170e5cf39d2226fc719d8f52cd98815
Maximilien Nowak <pro.mnowak@gmail.com>
2024-01-21 19:47:30 +0100

    opening windows with glfw

--- Ectoplasme.cpp
+++ Ectoplasme.cpp
@@ -3,18 +3,33 @@
 
 int main()
 {
-	glfwInit();
+	// safety check to see if glfw works
+	if (!glfwInit())
+	{
+		std::cerr << "Could not initialize GLFW!" << std::endl;
+		return 1;
+	}
+
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-	//define the window
+	// define the window
 	int width = 800;
 	int height = 600;
-	GLFWwindow* window = glfwCreateWindow(width, height, "Ectoplasme", nullptr, nullptr);
-	//main loop like in opengl
+	GLFWwindow *window = glfwCreateWindow(width, height, "Ectoplasme", nullptr, nullptr);
+
+	//safety check to see if the window is open
+	if (!window)
+	{
+		std::cerr << "Could not open window!" << std::endl;
+		glfwTerminate();
+		return 1;
+	}
+
+	// main loop like in opengl
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
 	}
-	
+
 	// clean up
 	glfwDestroyWindow(window);
 	glfwTerminate();
