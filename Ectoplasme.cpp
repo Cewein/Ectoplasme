abb6ea385d08bb3ba5267f518836763984c8cfd0
Maximilien Nowak <pro.mnowak@gmail.com>
2024-01-21 17:39:49 +0100

    remove opengl
    we are removing openGL to favorise webGPU.

--- Ectoplasme.cpp
+++ Ectoplasme.cpp
@@ -1,34 +1,6 @@
-﻿#include <GLFW/glfw3.h>
+﻿#include <iostream>
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-
-#include <iostream>
-#include <vector>
-
-int main()
-{
-	glfwInit();
-
-	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-
-	//define the window
-	int width = 800;
-	int height = 600;
-
-	GLFWwindow* window = glfwCreateWindow(width, height, "Ectoplasme", nullptr, nullptr);
-
-	//main loop like in opengl
-	while (!glfwWindowShouldClose(window))
-	{
-		glfwPollEvents();
-	}
-	
-	// clean up
-	glfwDestroyWindow(window);
-	glfwTerminate();
-
-	return 0;
+int main (int, char**) {
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
 }
